9c69d72c5496bd508ff908a034b2bb4e38289a16
R <ramz9_97@hotmail.com>
2021-11-05 00:43:01 +0200

    display fetched data to html

--- ajax.js
+++ ajax.js
@@ -14,4 +14,43 @@ form.addEventListener('submit', async (evt) => {
     const url = form.action + '?q=' + query.value;
     const tvShows = await fetchShow(url);
     console.log(tvShows);
+
+    // tvShows[0].show.name
+    const mainElement = document.querySelector('main');
+    mainElement.innerHTML = "";
+    
+    for (let i = 0; i < tvShows.length; i++){
+
+        mainElement.innerHTML +=
+        `<article>
+            <header>
+                <h2>${tvShows[i].show.name}</h2>
+            </header>
+            <figure>
+                <img src=${tvShows[i].show.image.medium} alt=${tvShows[i].show.name} 'image'>
+                <figcaption></figcaption>
+            </figure>
+            <p>${tvShows[i].show.summary}</p>
+        </article>`;
+    }
+
 });
+
+
+/*
+// Put code of task D here
+"use strict";
+for (let i = 0; i < picArray.length; i++){
+    const mainElement = document.querySelector("main");
+    mainElement.innerHTML += 
+    `<article>
+        <header>
+            <h2>${picArray[i].title}</h2>
+        </header>
+        <figure>
+            <img src=${picArray[i].filename} alt="title">
+            <figcaption>${picArray[i].caption}</figcaption>
+        </figure>
+        <p>${picArray[i].description}</p>
+    </article>`;
+} */
